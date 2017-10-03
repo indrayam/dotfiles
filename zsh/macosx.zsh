f4b62f328522df27327b79165a37ba467e184808
Anand Sharma <anand.sharma@gmail.com>
2017-10-03 15:22:55 -0400

    Cleaning up configurations (tmux learning)

--- zsh/macosx.zsh
+++ zsh/macosx.zsh
@@ -56,7 +56,7 @@ ulimit -n 8192
 alias p='python3'
 alias p2='python2'
 alias pipup='pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs pip3 install -U'
-alias g='go run'
+alias g='git'
 alias n='node'
 alias j='java'
 alias jc='javac'
@@ -78,7 +78,6 @@ alias d='docker'
 alias dm='docker-machine'
 alias dco='docker-compose'
 alias s='subl'
-alias b='brackets'
 
 # Aliases specific to being productive on MacOS X
 alias c='clear'
@@ -112,15 +111,5 @@ alias tk='tmux kill-session -t'
 alias tl='tmux ls'
 
 # Source: https://gist.github.com/bmhatfield/cc21ec0a3a2df963bffa3c1f884b676b
-# In order for gpg to find gpg-agent, gpg-agent must be running, and there must be an env
-# variable pointing GPG to the gpg-agent socket. This little script, which must be sourced
-# in your shell's init script (ie, .bash_profile, .zshrc, whatever), will either start
-# gpg-agent or set up the GPG_AGENT_INFO variable if it's already running.
-
 # Add the following to your shell init to set up gpg-agent automatically for every shell
-if [ -f ~/.gnupg/.gpg-agent-info ] && [ -n "$(pgrep gpg-agent)" ]; then
-    source ~/.gnupg/.gpg-agent-info
-    export GPG_AGENT_INFO
-else
-    eval $(gpg-agent --daemon --write-env-file ~/.gnupg/.gpg-agent-info)
-fi
+eval $(gpg-agent --daemon)
