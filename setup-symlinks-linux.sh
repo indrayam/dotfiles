263162e41be6652b9bc58a3df73f5859d5ddcffd
Anand Sharma <anand.sharma@gmail.com>
2017-10-07 18:28:43 -0400

    Final script related mistakes. Ready for take off!

--- setup-symlinks-linux.sh
+++ setup-symlinks-linux.sh
@@ -5,23 +5,23 @@ DOTFILES_HOME=$HOME/.dotfiles
 
 # Git related setup
 rm -f $HOME/.gitconfig
-ln -s $DOTFILES_HOME/git/gitconfig.basic .gitconfig
+ln -s $DOTFILES_HOME/git/gitconfig.basic $HOME/.gitconfig
 rm -f $HOME/.gitignore_global
-ln -s $DOTFILES_HOME/git/gitignore_global .gitignore_global
+ln -s $DOTFILES_HOME/git/gitignore_global $HOME/.gitignore_global
 
 # Vim
 rm -f $HOME/.vim
-ln -s $DOTFILES_HOME/vim .vim
+ln -s $DOTFILES_HOME/vim $HOME/.vim
 rm -f $HOME/.vimrc
-ln -s $DOTFILES_HOME/vim/vimrc .vimrc
+ln -s $DOTFILES_HOME/vim/vimrc $HOME/.vimrc
 
 # Tmux
 rm -f $HOME/.tmux.conf
-ln -s $DOTFILES_HOME/tmux/tmux.conf .tmux.conf
+ln -s $DOTFILES_HOME/tmux/tmux.conf $HOME/.tmux.conf
 
 # Zsh
 rm -f $HOME/.zshrc
-ln -s $DOTFILES_HOME/zsh/zshrc .zshrc.linux
+ln -s $DOTFILES_HOME/zsh/zshrc.linux $HOME/.zshrc
 
 
 
--- setup-symlinks-macosx.sh
+++ setup-symlinks-macosx.sh
@@ -5,20 +5,20 @@ DOTFILES_HOME=$HOME/.dotfiles
 
 # Git related setup
 rm -f $HOME/.gitconfig
-ln -s $DOTFILES_HOME/git/gitconfig .gitconfig
+ln -s $DOTFILES_HOME/git/gitconfig $HOME/.gitconfig
 rm -f $HOME/.gitignore_global
-ln -s $DOTFILES_HOME/git/gitignore_global .gitignore_global
+ln -s $DOTFILES_HOME/git/gitignore_global $HOME/.gitignore_global
 
 # Vim
 rm -f $HOME/.vim
-ln -s $DOTFILES_HOME/vim .vim
+ln -s $DOTFILES_HOME/vim $HOME/.vim
 rm -f $HOME/.vimrc
-ln -s $DOTFILES_HOME/vim/vimrc .vimrc
+ln -s $DOTFILES_HOME/vim/vimrc $HOME/.vimrc
 
 # Tmux
 rm -f $HOME/.tmux.conf
-ln -s $DOTFILES_HOME/tmux/tmux.conf .tmux.conf
+ln -s $DOTFILES_HOME/tmux/tmux.conf $HOME/.tmux.conf
 
 # Zsh
 rm -f $HOME/.zshrc
-ln -s $DOTFILES_HOME/zsh/zshrc .zshrc
+ln -s $DOTFILES_HOME/zsh/zshrc $HOME/.zshrc
