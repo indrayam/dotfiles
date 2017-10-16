9ff8cfe357fc0d6695b74ad20c63ed2934fa2419
Anand Sharma <anand.sharma@gmail.com>
2017-10-15 22:21:54 -0700

    Fixing Google Cloud SDK settings

--- zsh/plugins/macosx/macosx.plugin.zsh
+++ zsh/plugins/macosx/macosx.plugin.zsh
@@ -116,3 +116,9 @@ alias dco='doctl compute'
 eval $(gpg-agent --daemon)
 
 ulimit -n 8192
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/usr/local/google-cloud-sdk/path.zsh.inc' ]; then source '/usr/local/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/usr/local/google-cloud-sdk/completion.zsh.inc' ]; then source '/usr/local/google-cloud-sdk/completion.zsh.inc'; fi
