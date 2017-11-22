944fe646c478ff764313b7158d6919805369b01c
Anand Sharma <anand.sharma@gmail.com>
2017-11-22 09:40:32 -0500

    Cleaned up ssht function as it is unnecessary

--- zsh/plugins/macosx/macosx.plugin.zsh
+++ zsh/plugins/macosx/macosx.plugin.zsh
@@ -125,5 +125,3 @@ if [ -f '/usr/local/google-cloud-sdk/path.zsh.inc' ]; then source '/usr/local/go
 
 # The next line enables shell command completion for gcloud.
 if [ -f '/usr/local/google-cloud-sdk/completion.zsh.inc' ]; then source '/usr/local/google-cloud-sdk/completion.zsh.inc'; fi
-
-function ssht () {/usr/bin/ssh -t $@ "tmux attach || tmux new";}
