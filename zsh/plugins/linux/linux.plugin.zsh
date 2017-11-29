83cfdc781bbd90f12dbf2ee21d3bd641ebd5d838
Anand Sharma <anand.sharma@gmail.com>
2017-11-29 14:21:22 -0500

    Added an alias to run update

--- zsh/plugins/linux/linux.plugin.zsh
+++ zsh/plugins/linux/linux.plugin.zsh
@@ -85,3 +85,6 @@ if [ -f '/usr/local/google-cloud-sdk/path.zsh.inc' ]; then source '/usr/local/go
 
 # The next line enables shell command completion for gcloud.
 if [ -f '/usr/local/google-cloud-sdk/completion.zsh.inc' ]; then source '/usr/local/google-cloud-sdk/completion.zsh.inc'; fi
+
+# Alias for running update
+alias u="$HOME/.dotfiles/update-ubuntu.sh"
