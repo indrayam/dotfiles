18fc4b3c93490ff6f75fd7c17d09d48a9262fe17
Anand Sharma <anand.sharma@gmail.com>
2017-11-29 14:02:26 -0500

    Adding a script to update Ubuntu servers

---
+++ update-ubuntu.sh
@@ -0,0 +1,46 @@
+#!/bin/bash¬
+cd $HOME
+# Ubuntu Updates
+sudo apt update
+sudo apt upgrade
+sudo apt list --upgradeable
+# Python Updates
+pipup
+# Ruby Gem Update
+sudo gem update
+# NodeJS Update
+sudo npm -g --unsafe-perm
+cd $HOME/workspace/node-apps/helloapp
+npm update
+cd $HOME
+# AWS ECS CLI Update
+sudo curl -o /usr/local/bin/ecs-cli https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-linux-amd64-latest
+echo "$(curl -s https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-linux-amd64-latest.md5) /usr/local/bin/ecs-cli" | md5sum -c -
+sudo chmod 755 /usr/local/bin/ecs-cli
+cd /usr/local/bin
+sudo ln -s ./ecs-cli ./ecs
+# GCloud Update
+gcloud components update
+# AWS CLI Versions
+aws --version
+ecs --version
+# Google Cloud Versions
+gcloud --version
+# Azure Version
+az --version
+# Docker Version
+d version
+# Go Version
+go version
+# Python Version
+p --version
+pip3 --version
+# Ruby Version
+ruby --version
+gem --version
+# Node Version
+node --version
+npm --version
+# Java Version
+java --version
+mvn --version
