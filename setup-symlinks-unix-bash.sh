6ad13ff75824bc5914ebc9a4ff2fba0f728a4891
Anand Sharma <anasharm@cisco.com>
2020-01-28 08:48:34 -0500

    House Cleaning

--- setup-symlinks-unix-bash.sh
+++ setup-symlinks-unix-bash.sh
@@ -15,15 +15,11 @@ ln -s $DOTFILES_HOME/vim $HOME/.vim
 rm -f $HOME/.vimrc
 ln -s $DOTFILES_HOME/vim/vimrc $HOME/.vimrc
 
-# Tmux
-rm -f $HOME/.tmux.conf
-rm -f $HOME/.tmux.conf.local
-ln -s $DOTFILES_HOME/tmux/tmux.conf.linux $HOME/.tmux.conf
-ln -s $DOTFILES_HOME/tmux/tmux.conf.local.linux $HOME/.tmux.conf.local
-
 # Zsh
-rm -f $HOME/.zshrc
-ln -s $DOTFILES_HOME/zsh/zshrc.centos $HOME/.zshrc
+rm -f $HOME/.bash_profile
+rm -f $HOME/.bashrc
+ln -s $DOTFILES_HOME/bash/bashrc $HOME/.bash_profile
+ln -s $DOTFILES_HOME/bash/bashrc $HOME/.bashrc
 
 
 
