9cec544538d9e57ab8983ea8cbc84b75528488d9
Anand Sharma <anasharm@cisco.com>
2020-02-07 16:21:25 -0500

    Adding minishift config

--- zsh/plugins/macosx/macosx.plugin.zsh
+++ zsh/plugins/macosx/macosx.plugin.zsh
@@ -254,8 +254,10 @@ KUBE_PS1_SYMBOL_USE_IMG="true"
 KUBE_PS1_SYMBOL_COLOR="yellow"
 #KUBE_PS1_SYMBOL_IMG="☸️ "
 source "$HOME/.kube-ps1/kube-ps1.sh"
-
 kubeon
+
+## Local Kubernetes Configurations: Minishift
+export PATH="/Users/anasharm/.minishift/cache/oc/v3.11.0/darwin:$PATH"
 
 ################
 ### DevOps Tools
