2c7e1b309e7c1754a33b1ead6a4cade778e5965c
Anand Sharma <anasharm@cisco.com>
2024-10-08 12:54:07 -0500

    Cleaned up tmux references

--- setup-symlinks-linux.sh
+++ setup-symlinks-linux.sh
@@ -15,12 +15,6 @@ ln -s $DOTFILES_HOME/vim $HOME/.vim
 rm -f $HOME/.vimrc
 ln -s $DOTFILES_HOME/vim/vimrc $HOME/.vimrc
 
-# Tmux
-rm -f $HOME/.tmux.conf
-rm -f $HOME/.tmux.conf.local
-ln -s $DOTFILES_HOME/tmux/tmux.conf.linux $HOME/.tmux.conf
-ln -s $DOTFILES_HOME/tmux/tmux.conf.local.linux $HOME/.tmux.conf.local
-
 # Zsh
 rm -f $HOME/.zshrc
 ln -s $DOTFILES_HOME/zsh/zshrc.linux $HOME/.zshrc
